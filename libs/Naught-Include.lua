a4313eb69b3517ad06836d2ae1e95ca2e6a084b9
Joshua Tyree <joshuat@createthebridge.com>
2019-01-03 21:28:55 -0600

    Fixed anchor implementation

--- libs/Naught-Include.lua
+++ libs/Naught-Include.lua
@@ -121,63 +121,59 @@ function disable_blinkmenot()
   end
 end
 
--- Anchor = {}
--- Anchor.__index = Anchor
--- function Anchor:new()
---   o = o or {}
---   setmetatable(o, self)
---   self.__index = self
---
---   -- Begin Anchor Code
---   function bit(p)
---       return 2 ^ p
---   end
---
---   function checkbit(x, p)
---     return x % (p + p) >= p
---   end
---
---   function clearbit(x, p)
---     return checkbit(x, p) and x - p or x
---   end
---
---   function string.clearbits(s, p, c)
---     if c and c > 1 then
---       s = s:clearbits(p + 1, c - 1)
---     end
---     local b = math.floor(p / 8)
---     return s:sub(1, b) .. string.char(clearbit(s:byte(b + 1), bit(p % 8))) .. s:sub(b + 2)
---   end
---
---   function string.checkbit(s, p)
---     return checkbit(s:byte(math.floor(p / 8) + 1), bit(p % 8))
---   end
---
---   -- event callback functions
---   o.update = function(id, original, modified, injected, blocked)
---     if id == 0x28 then
---       local category = math.floor((original:byte(11) % 64) / 4)
---       if category == 11 then
---         local new = original
---         local position = 150
---         for target = 1,original:byte(10) do
---           new = new:clearbits(position + 60, 3)
---           local next_position = position + 123
---           if original:checkbit(position + 121) then
---             next_position = next_position + 37
---           end
---           if original:checkbit(position + 122) then
---             next_position = next_position + 34
---           end
---           position = next_position
---         end
---         return new
---       end
---     end
---   end
---   return o
--- end
---
+
+-- Begin Anchor Code
+function bit(p)
+    return 2 ^ p
+end
+
+function checkbit(x, p)
+  return x % (p + p) >= p
+end
+
+function clearbit(x, p)
+  return checkbit(x, p) and x - p or x
+end
+
+function string.clearbits(s, p, c)
+  if c and c > 1 then
+    s = s:clearbits(p + 1, c - 1)
+  end
+  local b = math.floor(p / 8)
+  return s:sub(1, b) .. string.char(clearbit(s:byte(b + 1), bit(p % 8))) .. s:sub(b + 2)
+end
+
+function string.checkbit(s, p)
+  return checkbit(s:byte(math.floor(p / 8) + 1), bit(p % 8))
+end
+
+-- event callback functions
+function check_packets(id, original, modified, injected, blocked)
+  if id == 0x28 then
+    local category = math.floor((original:byte(11) % 64) / 4)
+    if category == 11 then
+      local new = original
+      local position = 150
+      for target = 1,original:byte(10) do
+        new = new:clearbits(position + 60, 3)
+        local next_position = position + 123
+        if original:checkbit(position + 121) then
+          next_position = next_position + 37
+        end
+        if original:checkbit(position + 122) then
+          next_position = next_position + 34
+        end
+        position = next_position
+      end
+      return new
+    end
+  end
+end
+  return o
+end
+
+windower.raw_register_event('incoming chunk', )
+
 -- if anchor_instance == nil then
 --   anchor_instance = Anchor:new()
 --   windower.register_event('incoming chunk', anchor_instance.update)
